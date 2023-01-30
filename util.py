44cc1c51c80290361a800bdfcd59898684419248
chanwhistle <totoro321@naver.com>
2023-01-30 18:04:14 +0900

    minor change

--- util.py
+++ util.py
@@ -1,13 +1,10 @@
 #DataLoader
 
 # Load StpTokenizer models
-import argparse
 import os
-import re
 import torch
 from torch.utils.data import Dataset, DataLoader
 import sentencepiece as sp
-from torchtext.vocab import *
 
 
 # set variables
@@ -65,141 +62,55 @@ from torchtext.vocab import *
     
 #     args = parser.parse_args()
 
-    
-    # def build_vocab(Stp_de, Stp_en):
-        
-        
-    #     def tokenize_de(text):
-    #         return tokenize(text, Stp_de)
-
-    #     def tokenize_en(text):
-    #         return tokenize(text, Stp_en)
-    
-    #     print("Building German Vocabulary ...")
-    #     train, val, test = dataset_de.de_data(language_pair=("de", "en"))
-    #     vocab_src = build_vocab_from_iterator(
-    #         yield_tokens(train + val + test, tokenize_de, index=0),
-    #         min_freq=2,
-    #         specials=["<s>", "</s>", "<blank>", "<unk>"],
-    #     )
-
-    #     print("Building English Vocabulary ...")
-    #     train, val, test = dataset_en.en_data(language_pair=("de", "en"))
-    #     vocab_tgt = build_vocab_from_iterator(
-    #         yield_tokens(train + val + test, tokenize_en, index=1),
-    #         min_freq=2,
-    #         specials=["<s>", "</s>", "<blank>", "<unk>"],
-    #     )
-
-    #     vocab_src.set_default_index(vocab_src["<unk>"])
-    #     vocab_tgt.set_default_index(vocab_tgt["<unk>"])
-
-    #     return vocab_src, vocab_tgt
-    
-    
-    # def load_vocab(Stp_de, Stp_en):
-    #     if not os.path.exists("vocab.pt"):
-    #         vocab_src, vocab_tgt = build_vocabulary(Stp_de, Stp_en)
-    #         torch.save((vocab_src, vocab_tgt), "vocab.pt")
-    #     else:
-    #         vocab_src, vocab_tgt = torch.load("vocab.pt")
-    #     print("Finished.\nVocabulary sizes:")
-    #     print(len(vocab_src))
-    #     print(len(vocab_tgt))
-    #     return vocab_src, vocab_tgt
-
-
-
-
 
 class IWSLTDataset(Dataset):
-    def __init__(self, datapath = "./dataset/", src_lang = "de", trg_lang = "en", type = "train"):
+    def __init__(self, src_lang = "de", trg_lang = "en", type = "train"):
         super().__init__()
-        
-        SP = sp.SentencePieceProcessor()
-        
+         
         if type == "train":
-            datasets = ["IWSLT16"]
-            trg = []
-            src = []
-            for dataset in datasets:
-                trg_path = os.path.join(datapath, type, f"{type}.{trg_lang}")
-                src_path = os.path.join(datapath, type, f"{type}.{src_lang}")
-                with open(trg_path, encoding = "utf-8") as f:
-                    temp_trg = f.read().splitlines()
-                with open(src_path, encoding = "utf-8") as f:
-                    temp_src = f.read().splitlines()
-                trg += temp_trg
-                src += temp_src
-                   
-            self.trg_tok_list = self.tokenize("en_32000", SP, trg)
-            self.src_tok_list = self.tokenize("de_32000", SP, src)
+            trg, src = self.load_vocab(type, src_lang, trg_lang)
+            self.trg_tok_list = self.tokenize(f"{trg_lang}_32000", trg)
+            self.src_tok_list = self.tokenize(f"{src_lang}_32000", src)
+                      
                        
-        elif type == "tst" or type == "dev":
-            type = "tst"
-            datasets = ["IWSLT16.TED.tst2014", "IWSLT16.TED.tst2013", "IWSLT16.TED.tst2012", "IWSLT16.TED.tst2011", "IWSLT16.TED.tst2010"]
-            trg = []
-            src = []
-            for dataset in datasets:
-                trg_path = os.path.join(datapath, type, f"{dataset}.{src_lang}-{trg_lang}.{trg_lang}.xml")
-                src_path = os.path.join(datapath, type, f"{dataset}.{src_lang}-{trg_lang}.{src_lang}.xml")
-                with open(trg_path, encoding = "utf-8") as f:
-                    temp_trg = f.read().splitlines()
-                with open(src_path, encoding = "utf-8") as f:
-                    temp_src = f.read().splitlines()
-                assert(len(temp_src) == len(temp_trg))
-                trg += temp_trg
-                src += temp_src
-                
+        elif type == "tst":
+            trg, src = self.load_vocab(type, src_lang, trg_lang)
+            self.trg_tok_list = self.tokenize("en_32000", trg)
+            self.src_tok_list = self.tokenize("de_32000", src)
+        
+        
+        elif type == "dev":      
+            trg, src = self.load_vocab(type, src_lang, trg_lang)
             self.trg_tok_list = self.tokenize("en_32000", self.tokenizer(model="en_32000"), trg)
             self.src_tok_list = self.tokenize("de_32000", self.tokenizer(model="de_32000"), src)
-                
-            type = "dev"
-            datasets = ["IWSLT16.TED.dev2012", "IWSLT16.TED.dev2010"]
-            trg = []
-            src = []
-            for dataset in datasets:
-                trg_path = os.path.join(datapath, type, f"{dataset}.{src_lang}-{trg_lang}.{trg_lang}.xml")
-                src_path = os.path.join(datapath, type, f"{dataset}.{src_lang}-{trg_lang}.{src_lang}.xml")
-                with open(trg_path, encoding = "utf-8") as f:
-                    temp_trg = f.read().splitlines()
-                with open(src_path, encoding = "utf-8") as f:
-                    temp_src = f.read().splitlines()
-                assert(len(temp_trg) == len(temp_src))
-                trg += temp_trg
-                src += temp_src
-                
-            self.trg_tok_list = self.tokenize("en_32000", self.tokenizer(model="en_32000"), trg)
-            self.src_tok_list = self.tokenize("de_32000", self.tokenizer(model="de_32000"), src)
+        
+        else:
+            raise TypeError ("Data type is wrong. Please check data type.")
         
         self.src = src
         self.trg = trg
                 
+                
     def __len__(self):
         return len(self.src)
+
 
     def __getitem__(self,idx):
         en = torch.FloatTensor(self.trg_tok_list[idx])
         de = torch.FloatTensor(self.src_tok_list[idx])
         return en, de
     
+    
     def tokenizer(self, model):
-        if model == "en_32000":
-            SP_en = sp.SentencePieceProcessor.Load(model_file = 
-            "Tokenizer/models/"f"{model}.model")
-            return SP_en
-            
-        elif model =="de_32000":
-            SP_de = sp.SentencePieceProcessor.Load(model_file = 
-            "Tokenizer/models/"f"{model}.model")
-            return SP_de
+        SP = sp.SentencePieceProcessor()
+        SP_temp = SP.Load(model_file = 
+        "Tokenizer/models/tokenized_IWSLT_2016."f"{model}.model")
+        return SP
 
-        
-    def tokenize(self, model, tokenizer, vocab):
-        model_name = f"tokenized_IWSLT_2016.{model}"
+
+    def tokenize(self, model, vocab):
         tokenized = []      
-        tokenizer.Load(model_file = 
-            "Tokenizer/models/"f"{model_name}.model")
+        tokenizer = self.tokenizer(model)
         for lines in vocab:
             tokenized.append(tokenizer.EncodeAsIds(lines))
         
@@ -208,10 +119,54 @@ class IWSLTDataset(Dataset):
         for line in tokenized:
             while len(line) < max_len:
                 line.append(3)
-
         return tokenized
     
-dataset = IWSLTDataset()
+    
+    def load_vocab(self, type, src_lang, trg_lang):
+        datapath = "./dataset/"
+        trg_vocab = []
+        src_vocab = []
+        vocab_dict = {"train": ["IWSLT16.TED.train2014"],
+                      "tst": ["IWSLT16.TED.tst2014", "IWSLT16.TED.tst2013", "IWSLT16.TED.tst2012", "IWSLT16.TED.tst2011", "IWSLT16.TED.tst2010"],
+                      "dev": ["IWSLT16.TED.dev2012", "IWSLT16.TED.dev2010"]}
+        for dataset in vocab_dict.get(type):
+            trg_path = os.path.join(datapath, type, f"{dataset}.{src_lang}-{trg_lang}.{trg_lang}")
+            src_path = os.path.join(datapath, type, f"{dataset}.{src_lang}-{trg_lang}.{src_lang}")
+            with open(trg_path, encoding = "utf-8") as f:
+                temp_trg = f.read().splitlines()
+            with open(src_path, encoding = "utf-8") as f:
+                temp_src = f.read().splitlines()
+            assert(len(trg_vocab) == len(src_vocab))
+                
+            print("\nLoaded Vocab! \nVocab sizes:")
+            trg_vocab += temp_trg
+            print("target vocab : ", len(trg_vocab))
+            src_vocab += temp_src
+            print("Source vocab : ", len(src_vocab))    
+        return trg_vocab, src_vocab
+    
+    def build_vocab(self):
+        SP_trainer = sp.SentencePieceTrainer
+        user_defined_symbols = ["<PAD>", "<UNK>", "<EOS>", "<BOS>", "<CLS>", "<SEP>", "<MASK>"]
+        vocab_size = 32000
+        model_prefix = f"{}"
+        trg_lang = "en"
+        src_lang = "de"
+        input = 
+        
+        print("Building German Vocabulary...")
+        de_model = SP_trainer.Train("--input = , --model_prefix = , --vocab_size = , --user_defined_symbols = , --model_type = , --character_coverage = ")
+        
+        print("Building English Vocabulary...")
+        en_model = SP_trainer.Train("--input = , --model_prefix = , --vocab_size = , --user_defined_symbols = , --model_type = , --character_coverage = ")
+        
+        
+        print("Voacb Training Finished!")
+
+    
+dataset = IWSLTDataset(type="tst")
+
+print("Dataset Loaded")
 
 dataloader = DataLoader(
     dataset,
