b4f90dc55f0d960c9aa2c01920c978155c327ba2
chanwhistle <totoro321@naver.com>
2023-02-09 19:57:16 +0900

    dataloader

--- Dataloader.py
+++ Dataloader.py
@@ -4,13 +4,10 @@ import os
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pad_sequence
-from torchtext.data import Field, BucketIterator
 import sentencepiece as sp
 from tqdm import tqdm
 import pickle
-import tarfile
-from urllib.request import urlretrieve
-import urllib.request
+from utils import *
 
 class CustomDataset(Dataset):
     def __init__(self,
@@ -32,41 +29,77 @@ class CustomDataset(Dataset):
         self.pad = "<pad>"
         self.bos = "<bos>"
         self.eos = "<eos>"
+        self.specials={
+                self.unk: self.unk_idx,
+                self.pad: self.pad_idx,
+                self.bos: self.bos_idx,
+                self.eos: self.eos_idx
+                }
         self.data_path = data_path
-
-        trg, src = self.load_vocab("train")
-        self.train_trg_tok_list = self.tokenize(f"{self.trg_lang}_32000", trg, "train")
-        self.train_src_tok_list = self.tokenize(f"{self.src_lang}_32000", src, "train")
-        print("builded train dataset")
-
-        trg, src = self.load_vocab("test")
-        self.test_trg_tok_list = self.tokenize(f"{self.trg_lang}_32000", trg, "test")
-        self.test_src_tok_list = self.tokenize(f"{self.src_lang}_32000", src, "test")
-        print("builded test dataset")
-     
-        trg, src = self.load_vocab("dev")
-        self.dev_trg_tok_list = self.tokenize(f"{self.trg_lang}_32000", trg, "dev")
-        self.dev_src_tok_list = self.tokenize(f"{self.src_lang}_32000", src, "dev")
-        print("builded dev dataset")
         
-        self.src = src
+        self.vocab_src = None
+        self.vocab_tgt = None
 
+        train_file = os.path.join(data_path, "train.pickle")
+        dev_file = os.path.join(data_path, "dev.pickle")
+        test_file = os.path.join(data_path, "test.pickle")
+
+        if os.path.exists(train_file):
+            self.train = load_pkl(train_file)
+        else:
+            trg_train, src_train = self.load_corpus("train")
+            self.train = [(en, de) for en, de in zip(trg_train, src_train)]
+            save_pkl(self.train , train_file)
+            
+        self.train_trg_tok = self.tokenize(f"{self.trg_lang}_32000",
+                                                self.train[0], "train")
+        self.train_src_tok = self.tokenize(f"{self.src_lang}_32000",
+                                                self.train[1], "train")
+                
+        self.train_t = [(en, de) for en, de in zip(self.to_tensor(self.train_trg_tok), self.to_tensor(self.train_src_tok))]
+        print("built train dataset")
+        
+        
+        if os.path.exists(dev_file):
+            self.dev = load_pkl(dev_file)
+        else:
+            trg_dev, src_dev = self.load_corpus("dev")
+            self.dev = [(en, de) for en, de in zip(trg_dev, src_dev)]
+            save_pkl(self.dev , dev_file)
+            
+        self.dev_trg_tok = self.tokenize(f"{self.trg_lang}_32000",
+                                              self.dev[0], "dev")
+        self.dev_src_tok = self.tokenize(f"{self.src_lang}_32000",
+                                              self.dev[1], "dev")
+        self.dev_t = [(en, de) for en, de in zip(self.to_tensor(self.dev_trg_tok), self.to_tensor(self.dev_src_tok))]
+        print("built dev dataset")
+            
+            
+        if os.path.exists(test_file):
+            self.test = load_pkl(test_file)
+        else:
+            trg_test, src_test = self.load_corpus("dev")
+            self.test = [(en, de) for en, de in zip(trg_test, src_test)]
+            save_pkl(self.test , test_file)
+            
+        self.test_trg_tok = self.tokenize(f"{self.trg_lang}_32000",
+                                               self.test[0], "test")
+        self.test_src_tok = self.tokenize(f"{self.src_lang}_32000",
+                                               self.test[1], "test")
+        self.test_t = [(en, de) for en, de in zip(self.to_tensor(self.test_trg_tok), self.to_tensor(self.test_src_tok))]
+        print("built test dataset")
+    
 
     def __len__(self):
 
-        return len(self.src)
+        return len(self.train_trg_tok)
 
 
     def __getitem__(self,idx):
-        train_en = torch.IntTensor(self.train_trg_tok_list[idx])      # dataset[0] = encoded train_en data
-        train_de = torch.IntTensor(self.train_src_tok_list[idx])      # dataset[1] = encoded train_de data
-        test_en = torch.IntTensor(self.test_trg_tok_list[idx])        # dataset[2] = encoded test_en data
-        test_de = torch.IntTensor(self.test_src_tok_list[idx])        # dataset[3] = encoded test_de data
-        dev_en = torch.IntTensor(self.dev_trg_tok_list[idx])          # dataset[4] = encoded dev_en data
-        dev_de = torch.IntTensor(self.dev_src_tok_list[idx])          # dataset[5] = encoded dev_de data
-
-        return train_en, train_de, test_en, test_de, dev_en, dev_de
-
+        train_en = torch.IntTensor(self.train_trg_tok[idx])
+        train_de = torch.IntTensor(self.train_src_tok[idx])  # dataset[0] = encoded train_en data
+       
+        return train_en, train_de,
 
     def tokenizer(self, model):
         SP = sp.SentencePieceProcessor()
@@ -76,7 +109,6 @@ class CustomDataset(Dataset):
             self.build_vocab()
             SP_temp = SP.Load(model_file = 
             "Tokenizer/vocab/"f"{model}/"f"{model}.model")
-        
         else:
             pass
                   
@@ -107,9 +139,9 @@ class CustomDataset(Dataset):
         return tokenized
 
 
-    def load_vocab(self, type):
-        trg_vocab, src_vocab = [], []
-        vocab_dict = {"train": ["IWSLT16.TED.train"],
+    def load_corpus(self, type):
+        trg_corpus, src_corpus = [], []
+        corpus_dict = {"train": ["IWSLT16.TED.train"],
                       
                       "test": ["IWSLT16.TEDX.tst2014", 
                                "IWSLT16.TEDX.tst2013", 
@@ -122,23 +154,23 @@ class CustomDataset(Dataset):
                       "dev": ["IWSLT16.TED.dev2010", 
                               "IWSLT16.TEDX.dev2012"]}
         
-        for dataset in vocab_dict.get(type):
+        for corpus in corpus_dict.get(type):
             trg_path = os.path.join(self.data_path, type, 
-                                    f"{dataset}.{self.src_lang}-{self.trg_lang}.{self.trg_lang}.xml")
+                                    f"{corpus}.{self.src_lang}-{self.trg_lang}.{self.trg_lang}.xml")
             src_path = os.path.join(self.data_path, type, 
-                                    f"{dataset}.{self.src_lang}-{self.trg_lang}.{self.src_lang}.xml")
+                                    f"{corpus}.{self.src_lang}-{self.trg_lang}.{self.src_lang}.xml")
             with open(trg_path, encoding = "utf-8") as f:
                 temp_trg = f.read().splitlines()
             with open(src_path, encoding = "utf-8") as f:
                 temp_src = f.read().splitlines()
             # assert(len(temp_trg) == len(temp_src)), "Vocab size is different!!"
-            trg_vocab += temp_trg
-            src_vocab += temp_src
-        print("Loaded Vocab! \nVocab sizes:")
-        print("target vocab : ", len(trg_vocab))
-        print("Source vocab : ", len(src_vocab))   
+            trg_corpus += temp_trg
+            src_corpus += temp_src
+        print("Loaded Corpus! \nCorpus sizes:")
+        print("target Corpus : ", len(trg_corpus))
+        print("source Corpus : ", len(src_corpus))   
 
-        return trg_vocab, src_vocab
+        return trg_corpus, src_corpus
     
             
     def build_vocab(self):
@@ -188,38 +220,32 @@ class CustomDataset(Dataset):
             Stp_trainer.Train(input)
             print(f"{lang} Vocab Generation Finished")
             
-
-    def train_collate_fn(self, samples):
+            
+    def to_tensor(self, sentences):
+        tensor_sent = []
+        for sentence in tqdm(sentences):
+            T = torch.IntTensor(sentence)
+            tensor_sent.append(T)
+        return tensor_sent
+            
+            
+    def my_collate_fn(self, samples):
         trg = [sample[0] for sample in samples]
         src = [sample[1] for sample in samples]
-        padded_trg_train = pad_sequence(trg, batch_first=True, padding_value = int(self.pad_idx))  # Padding
-        padded_src_train = pad_sequence(src, batch_first=True, padding_value = int(self.pad_idx))  # Padding
-        return {'padded_trg_train': padded_trg_train.contiguous(),
-                'padded_src_train': padded_src_train.contiguous()}
-        
-        
-    def test_collate_fn(self, samples):
-        trg = [sample[2] for sample in samples]
-        src = [sample[3] for sample in samples]
-        padded_trg_test = pad_sequence(trg, batch_first=True, padding_value = int(self.pad_idx))  # Padding
-        padded_src_test = pad_sequence(src, batch_first=True, padding_value = int(self.pad_idx))  # Padding
-        return {'padded_trg_train': padded_trg_test.contiguous(),
-                'padded_src_train': padded_src_test.contiguous()}
-        
-        
-    def dev_collate_fn(self, samples):
-        trg = [sample[4] for sample in samples]
-        src = [sample[5] for sample in samples]
-        padded_trg_dev = pad_sequence(trg, batch_first=True, padding_value = int(self.pad_idx))  # Padding
-        padded_src_dev = pad_sequence(src, batch_first=True, padding_value = int(self.pad_idx))  # Padding
-        return {'padded_trg_train': padded_trg_dev.contiguous(),
-                'padded_src_train': padded_src_dev.contiguous()}
-        
+        batch_trg = pad_sequence(trg, batch_first=True, padding_value = int(self.pad_idx))  # Padding
+        batch_src = pad_sequence(src, batch_first=True, padding_value = int(self.pad_idx))  # Padding
+        return (batch_src, batch_trg)       
+
         
     def get_iter(self, **kwargs):
-        train_iter = DataLoader(CustomDataset, collate_fn=self.train_collate_fn, **kwargs)
-        valid_iter = DataLoader(CustomDataset, collate_fn=self.test_collate_fn, **kwargs)
-        test_iter = DataLoader(CustomDataset, collate_fn=self.dev_collate_fn, **kwargs)
-        return train_iter, valid_iter, test_iter
-
-dataset=CustomDataset()
+        train_iter = DataLoader(self.train_t,
+                                collate_fn=self.my_collate_fn,
+                                **kwargs)
+        dev_iter = DataLoader(self.dev_t,
+                                collate_fn=self.my_collate_fn,
+                                **kwargs)
+        test_iter = DataLoader(self.test_t,
+                               collate_fn=self.my_collate_fn,
+                               **kwargs)
+        return train_iter, dev_iter, test_iter
+    
